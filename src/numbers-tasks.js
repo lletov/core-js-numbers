c60f2fe2f6a12274051bd7646907648b5e3f14b1
Dmitry <dmitry2595@gmail.com>
2024-12-12 13:38:14 +0300

    feat: 8 pass

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -178,8 +178,13 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return parseInt(
+    Math.round(num / 10 ** pow)
+      .toString()
+      .split('.')[0] + Array(pow).fill('0').join(''),
+    10
+  );
 }
 
 /**
